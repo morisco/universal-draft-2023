14be8834c5a5d55b6e437cf03f2bc6bf249ab955
Mike Morisco <mikemorisco@gmail.com>
2023-02-01 19:29:47 -0500

    Updating nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
   buildModules: [
     '@nuxtjs/moment',
     ['@nuxtjs/dotenv', { 
-      filename: '.env.staging' 
+      filename: '.env' 
     }],
     // '@nuxtjs/google-analytics',
   ],
