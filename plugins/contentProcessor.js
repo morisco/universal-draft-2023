26a056e189018f83a63713231c51e4c473d00084
Mike Morisco <mikemorisco@gmail.com>
2023-03-01 21:43:01 -0500

    Update contentProcessor.js

--- plugins/contentProcessor.js
+++ plugins/contentProcessor.js
@@ -34,13 +34,12 @@ function processImages(image) {
 function processOrders(players) {
   const orderIds = players.map((player) => { return {id: player.id, bigBoard: parseInt(player.order,10), mockDraft: parseInt(player.order_mockdraft,10), draftResults: parseInt(player.order_draftresults,10) } });
   const bigBoardSorted = orderIds.sort((playerA, playerB) => (playerA.bigBoard > playerB.bigBoard) ? 1 : -1);
-  const mockIds = orderIds.filter(player => player.mockDraft <= 31);
   const draftResultIds = players.filter(player => player.drafted_team);
-  const mockDraftSorted = mockIds.sort((playerA, playerB) => (playerA.mockDraft > playerB.mockDraft) ? 1 : -1);
+  const mockDraftSorted = orderIds.sort((playerA, playerB) => (playerA.mockDraft > playerB.mockDraft) ? 1 : -1);
   const draftResultsSorted = draftResultIds.sort((playerA, playerB) => (playerA.order_draftresults > playerB.order_draftresults) ? 1 : -1);
   return {
     bigBoard: bigBoardSorted.map(player => player.id),
-    mockDraft: mockDraftSorted.map(player => player.id),
+    mockDraft: mockDraftSorted.slice(0,31).map(player => player.id),
     draftResults: draftResultsSorted.map(player => player.id)
   };
 }
